487b771699df021ef5132cb2fb1dbc94b202ff29
R Searls <rsearls@redhat.com>
2022-11-07 10:14:37 -0500

    enable more debug

--- server-adapters/resteasy-reactor-netty/src/test/java/org/jboss/resteasy/test/StreamingOutputTest.java
+++ server-adapters/resteasy-reactor-netty/src/test/java/org/jboss/resteasy/test/StreamingOutputTest.java
@@ -40,8 +40,8 @@ import org.jboss.byteman.contrib.bmunit.BMScript;
 import org.jboss.byteman.contrib.bmunit.BMUnitConfig;
 import org.junit.runner.RunWith;
 @RunWith(org.jboss.byteman.contrib.bmunit.BMUnitRunner.class)
-@BMUnitConfig(loadDirectory="target/test-classes")
-//@BMUnitConfig(loadDirectory="target/test-classes", debug=true, verbose=true, bmunitVerbose = true)
+//@BMUnitConfig(loadDirectory="target/test-classes")
+@BMUnitConfig(loadDirectory="target/test-classes", debug=true, verbose=true, bmunitVerbose = true)
 @BMScript(value="check.btm")
 // rls end byteman debug .. to be removed
 /**
